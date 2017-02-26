50f8e8d0faf9486711a4ba8faca6641a00650e4b
Brian Fakhoury <brian@dhcp-wifi-8021x-155-41-72-197.bu.edu>
2017-02-26 12:35:26 -0500

    new files

--- popup.js
+++ popup.js
@@ -1,3 +1,2 @@
-var pictureChange = function() {
-  document.getElementById('myImage').src='default.gif';
-}
+document.getElementById("button1").addEventListener("click", function(){
+  document.getElementById("myImage").src = "default.gif";});
