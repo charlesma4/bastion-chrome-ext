bb205f50b0d396e27b8896e482e049cffe63546e
Brian Fakhoury <brian@dhcp-wifi-8021x-155-41-24-43.bu.edu>
2017-02-28 11:41:14 -0500

    organized

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,5 @@
 document.getElementById("button1").addEventListener("click", function(){
-  document.getElementById("myImage").src = "switch.gif";
+  document.getElementById("myImage").src = "images/switch.gif";
   document.getElementById('button1').textContent="Verifying"
   setTimeout(function() {window.location.href='landing.html'}, 2000);
 });
